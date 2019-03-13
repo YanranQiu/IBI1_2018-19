9920c29dbb5b3caef936744b0f576200c0541579
Yanran Qiu <yanran.18@intl.zju.edu.cn>
2019-03-13 19:21:37 +0800

    Update powersof2.py

--- Practical4/powersof2.py
+++ Practical4/powersof2.py
@@ -6,23 +6,22 @@ Created on Wed Mar 13 10:43:28 2019
 """
 
 #start with a number no larger than 8192
-x=2019
-#set k as the exponent
-k=0
+x=int(input())
 #set the cumulative result
 r=str(x)+" is "
 #set the loop
 while x>0:
-    a=1
+    a=2**0
+#set k as the exponent
+    k=0
+#find the biggest exponent
     while a<=x:
         a=a*2
         k=k+1
+#print the exponent in the result
     k=k-1
-    print("2**"+str(k))
     x=x-a/2
     if r!=0:
         r=r+"2**"+str(k)+"+"
-    else:
-        r=r+"2**"+str(k)
 #print the final result
 print(r)
