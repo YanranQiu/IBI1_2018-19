b7cf0942ebd6d3103b192e170b3a6ced71fd7694
Yanran Qiu <yanran.18@intl.zju.edu.cn>
2019-03-13 14:18:37 +0800

    Update powersof2.py

--- Practical4/powersof2.py
+++ Practical4/powersof2.py
@@ -7,18 +7,22 @@ Created on Wed Mar 13 10:43:28 2019
 
 #start with a number no larger than 8192
 x=2019
-#set the power counter k
-k=13
+#set k as the exponent
+k=0
+#set the cumulative result
+r=str(x)+" is "
 #set the loop
-while 1==1:
-    if (x-2**k)<0:
-        k=k-1
-        continue
-    elif (x-2**k)>=0:
-        print( "2**", k, "+") 
-        x=x-2**k
-        list.append(k)
-        k=k-1
-        continue
+while x>0:
+    a=1
+    while a<=x:
+        a=a*2
+        k=k+1
+    k=k-1
+    print("2**"+str(k))
+    x=x-a/2
+    if r!=0:
+        r=r+"2**"+str(k)+"+"
+    else:
+        r=r+"2**"+str(k)
 #print the final result
-print("2019 is ", )
+print(r)
