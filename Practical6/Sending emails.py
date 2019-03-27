b8280633d1989e36fa8ac558539f1d88d62ce49b
Yanran Qiu <yanran.18@intl.zju.edu.cn>
2019-03-27 09:14:05 +0800

    Add Practical6 folder and Sending emails.py

---
+++ Practical6/Sending emails.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 27 09:06:02 2019
+
+@author: alvaq
+"""
+
+print("Dear User,\n\nPlease find the results of your gene set linkage analysis result in attached file.\n\nThis is an email sent by the server, please don't reply.\n\nThank you for using GSLA.")
