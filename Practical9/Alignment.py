fc21166c713086c56401d758cec57eef9b10b0f3
Yanran Qiu <yanran.18@intl.zju.edu.cn>
2019-05-16 19:48:19 +0800

    Update Practical 9 files

--- Practical9/Alignment.py
+++ Practical9/Alignment.py
@@ -58,17 +58,23 @@ for i in range(len(random_seq)):
     scr3=scr3+scr
     perc_3=perc3/len(random_seq)
     
+#bonus project (graphical alignment)
+from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
+for a in pairwise2.align.globaldx(SOD2_human_seq, SOD2_mouse_seq, BLOSUM62):
+    print(format_alignment(*a))
+for a in pairwise2.align.globaldx(SOD2_human_seq, random_seq, BLOSUM62):
+    print(format_alignment(*a))
+for a in pairwise2.align.globaldx(SOD2_mouse_seq, random_seq, BLOSUM62):
+    print(format_alignment(*a))
+
+#normalize BLOSUM score
+nscr1=scr1/len(SOD2_human_seq)
+nscr2=scr2/len(SOD2_human_seq)
+nscr3=scr3/len(random_seq)
+
 #print out the analyzed sequences and the final BLOSUM score
 print("The final BLOSUM score for human and mouse is "+str(scr1)+".\nThe final BLOSUM score for human and random is "+str(scr2)+".\nThe final BLOSUM score for mouse and random is "+str(scr3)+".")
+print("The normalized BLOSUM score for human and mouse is "+str(nscr1)+".\nThe normalized BLOSUM score for human and random is "+str(nscr2)+".\nThe normalized BLOSUM score for mouse and random is "+str(nscr3)+".")
 #print out the percentage identity
 print("The percentage identity for human and mouse is "+str(perc_1)+".\n"+"The percentage identity for human and random is "+str(perc_2)+".\n"+"The percentage identity for mouse and random is "+str(perc_3)+".")
-
-##for bonus project
-#from Bio import pairwise2
-#from Bio.pairwise2 import format_alignment
-#for a in pairwise2.align.globaldx(SOD2_human_seq, SOD2_mouse_seq, BLOSUM62):
-#    print(format_alignment(*a))
-#for a in pairwise2.align.globaldx(SOD2_human_seq, random_seq, BLOSUM62):
-#    print(format_alignment(*a))
-#for a in pairwise2.align.globaldx(SOD2_mouse_seq, random_seq, BLOSUM62):
-#    print(format_alignment(*a))
